4ecc8fff88795e824325f526d39ff9f4b0df8f15
bvtuan <bachvtuan@gmail.com>
2012-09-12 16:56:45 +0700

    add layout file.

---
+++ module/sublayout/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blog Mongo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <div id="top_panel">
+            <a href="?page=index">Index</a>
+            <a href="?page=new">New post</a>
+        </div>
